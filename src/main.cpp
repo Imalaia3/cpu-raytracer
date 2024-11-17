5b44ba2ec1b8eef6ebc3df59e111dbb0d86a261b
Imalaia3 <70742174+Imalaia3@users.noreply.github.com>
2024-11-17 10:28:30 +0200

    Add CMakeLists.txt and a starting main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <glm/glm.hpp>
+#include <SDL2/SDL.h>
+
+int main(int argc, char const *argv[]) {   
+    SDL_version ver; SDL_VERSION(&ver);
+    std::cout << "SDL2 Version: " << (uint32_t)ver.major << "." << (uint32_t)ver.minor << "\n";
+    std::cout << "GLM Version: " << GLM_VERSION << "\n";
+
+    return 0;
+}
